65658cafa8d98d9d509234816f49918b6bea29f2
Victor <vic.kunde@gmail.com>
2024-03-12 13:44:57 -0300

    Fixed security group

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -15,22 +15,7 @@ resource "aws_security_group" "allow_tls" {
     Name = "Terraform_SG"
   }
 
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  
   ingress {
     description      = "SSH"
     from_port        = 22
@@ -40,25 +25,9 @@ resource "aws_security_group" "allow_tls" {
     ipv6_cidr_blocks = ["::/0"]
   }
   ingress {
-    description      = "Streamlit"
-    from_port        = 8501
-    to_port          = 8501
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "MySQL"
-    from_port        = 3306
-    to_port          = 3306
+    description      = "RabbitMQ"
+    from_port        = 15672
+    to_port          = 15672
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
